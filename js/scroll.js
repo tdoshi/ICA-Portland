fbd7d450eacc01632a3a2ef09b2b1f87f1f2d0db
Tulsee Doshi <tulsee27@gmail.com>
2015-03-15 14:46:30 -0700

    adding files

---
+++ js/scroll.js
@@ -0,0 +1,14 @@
+console.log('hrllo eorlds');
+$( document ).ready(function() {
+	var $root = $('html, body');
+	$('a').click(function() {
+	    var href = $.attr(this, 'href')
+	    var nameHref = '[name="' + href.substr(1) + '"]';	    
+	    $root.animate({
+	        scrollTop: $(nameHref).offset().top
+	    }, 500, function () {
+	        window.location.hash = href;
+	    });
+	    return false;
+	});
+});
